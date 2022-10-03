511bb38272adadc6842d7f0ba1d1d2d5e69ede7c
Thomas <thomas@rogstrix.localdomain>
2022-10-03 12:37:57 +0200

    update GPU support

--- src/domain/steps/mlflow_trainer.py
+++ src/domain/steps/mlflow_trainer.py
@@ -23,6 +23,8 @@ class TrainClassifierConfig(BaseStepConfig):
     loss: str = "categorical_crossentropy"
     nb_epochs: int = 2
     seed: int = 42
+    allow_gpu: bool = False
+    memory_limit: int = 0
 
 
 @enable_mlflow
@@ -31,6 +33,40 @@ def train_classifier(
     config: TrainClassifierConfig,
     train_df: pd.DataFrame,
 ) -> Output(model=tf.keras.Model):
+
+    # Check if GPU available
+    print("GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+    print("GPUs allowed: {}".format(config.allow_gpu))
+    print("GPUS memory limit: {} MB".format(config.memory_limit))
+    gpus = tf.config.list_physical_devices('GPU')
+
+    if gpus:
+
+        if not config.allow_gpu:
+            print("GPUs not allowed for this run. Disabling...")
+            try:
+                # Disable first GPU
+                tf.config.set_visible_devices(gpus[1:], 'GPU')
+                logical_devices = tf.config.list_logical_devices('GPU')
+                # Logical device was not created for first GPU
+                assert len(logical_devices) == len(gpus) - 1
+                print("Tensorflow visible GPU: {}".format(len(logical_devices)))
+            except Exception as e:
+                # Invalid device or cannot modify virtual devices once initialized.
+                print("Impossible to disable first GPU: {}".format(e))
+                pass
+        else:
+        # Restrict TensorFlow to only allocate certain amount of memory on the first GPU
+            print("GPUs allowed for this run. Setting memory limit...")
+            try:
+                tf.config.experimental.set_virtual_device_configuration(
+                    gpus[0],
+                    [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=config.memory_limit)])
+                logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+                print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+            except Exception as e:
+                # Virtual devices must be set before GPUs have been initialized
+                print("Impossible to set memory limit: {}".format(e))
 
     # Train data generator
     train_generator = ImageDataGenerator(validation_split=config.validation_split)
