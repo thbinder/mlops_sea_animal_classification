c62e7ba33f1fbf9e2b61c7e37c51615cda784213
Thomas <thomas@rogstrix.localdomain>
2022-10-04 20:55:32 +0200

    update

--- src/domain/pipeline/training_kubeflow.py
+++ src/domain/pipeline/training_kubeflow.py
@@ -5,10 +5,6 @@ from absl import logging as absl_logging
 from dotenv import load_dotenv
 from kubernetes import client as k8s_client
 from zenml.config.docker_configuration import DockerConfiguration
-from zenml.integrations.kubeflow.orchestrators.kubeflow_orchestrator import (
-    KubeflowEntrypointConfiguration,
-    KubeflowOrchestrator,
-)
 from zenml.pipelines import pipeline
 
 from src.domain.steps.data_loader import train_data_loader
@@ -17,23 +13,21 @@ from src.domain.steps.evaluator import evaluate_classifier
 from src.domain.steps.skew_detector import evidently_skew_detector
 from src.domain.steps.trainer import train_classifier
 
+
+## Uncomment below to allow working with kubeflow multi tenancy
+# from zenml.integrations.kubeflow.orchestrators.kubeflow_orchestrator import (
+#     KubeflowEntrypointConfiguration,
+#     KubeflowOrchestrator,
+# )
 # original = KubeflowOrchestrator._configure_container_op
-
-
 # def patch_container_op(container_op):
 #     original(container_op)
 #     container_op.container.add_env_variable(
 #         k8s_client.V1EnvVar(name="ZENML_RUN_NAME", value="{{workflow.name}}")
 #     )
-
-
 # KubeflowOrchestrator._configure_container_op = staticmethod(patch_container_op)
-
-
 # def patch_get_run_name(self, pipeline_name):
 #     return os.getenv("ZENML_RUN_NAME")
-
-
 # KubeflowEntrypointConfiguration.get_run_name = patch_get_run_name
 
 # Configure warnings
