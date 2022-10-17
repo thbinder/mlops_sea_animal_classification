4d93832c25b45bbc7791400836d54cc3fc1d2b0b
Thomas <thomas@rogstrix.localdomain>
2022-10-17 11:47:03 +0200

    update tests

--- tests/api/app_test.py
+++ tests/api/app_test.py
@@ -11,8 +11,16 @@ def test_ping():
     assert response.status_code == 200
     assert response.json() == {"message": "pong!"}
 
+def test_predict_without_authentication():
 
-def test_predict():
+    _test_upload_file = "./test_data/nudibranch.jpg"
+    response = client.post(
+        "/predict",
+        files={"file": ("filename", open(_test_upload_file, "rb"), "image/jpeg")},
+    )
+    assert response.status_code == 401
+
+def test_predict_with_authentication():
 
     _test_upload_file = "./test_data/nudibranch.jpg"
     auth = HTTPBasicAuth(username="thomas", password="thomas")
