8897af84f93e8982d0c3cfc0b5ffcc45b7d7c8f9
thbinder <thomas@rogstrix>
2023-02-08 17:22:23 +0100

    update

--- tests/app/app_test.py
+++ tests/app/app_test.py
@@ -23,7 +23,7 @@ def test_predict_without_authentication():
 
 def test_predict_with_authentication():
     _test_upload_file = "./tests_data/nudibranch.jpg"
-    auth = HTTPBasicAuth(username="thomas", password="thomas")
+    auth = HTTPBasicAuth(username="test_user", password="test_password")
     response = client.post(
         "/predict",
         files={"file": ("filename", open(_test_upload_file, "rb"), "image/jpeg")},
